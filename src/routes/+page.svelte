f2119c168d7f36ce4c1b6b72d3b967a1e0a42216
fadelyking <fadely195@gmail.com>
2024-09-05 22:42:05 +0300

    create components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,7 @@
     import "../app.css";
 </script>
 <slot />
-<h1 class=" text-3xl bg-red-900">Welcome to SvelteKit</h1>
+<h1 class=" text-4xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
