21a20e7ee2935fcfea086b55bf099e9555e502f5
fadelyking <fadely195@gmail.com>
2024-09-05 22:20:55 +0300

    tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import "../app.css";
+</script>
+<slot />
+<h1 class=" text-3xl bg-red-900">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
