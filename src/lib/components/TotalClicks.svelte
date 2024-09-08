463f58cb9b17c69efa86cf0a2a20792b16998534
fadelyking <fadely195@gmail.com>
2024-09-09 02:38:23 +0300

    improved look of total clicks

--- src/lib/components/TotalClicks.svelte
+++ src/lib/components/TotalClicks.svelte
@@ -7,11 +7,19 @@
 	});
 </script>
 
-<div class="clicks-num">{total} Clicked</div>
+<div class="clicks-num font-bold flex flex-col items-center justify-center">
+	<span class="mb-10">JOIN THE MOVEMENT!</span>
+	<span class="num px-3">{total}</span> Clicks
+</div>
 
 <style>
 	.clicks-num {
 		color: white;
-		font-size: 60px;
+		font-size: 50px;
+	}
+
+	.num {
+		background-color: #0e696a;
+		border-radius: 12px;
 	}
 </style>
