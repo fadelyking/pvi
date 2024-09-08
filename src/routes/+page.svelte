90633be5be1f55dd42c880d10140edfe4bc2bcac
fadelyking <fadely195@gmail.com>
2024-09-09 02:38:43 +0300

    improved look of background and centered elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 			<Country countryName="Palestine">Palestine</Country>
 		</div>
 
-		<div class="flex flex-col items-center justify-center gap-5">
+		<div class="flex flex-col items-center justify-center gap-5 mb-36">
 			<TotalClicks />
 			<ProgressBar />
 		</div>
@@ -20,3 +20,51 @@
 		</div>
 	</div>
 </div>
+
+<div class="color"></div>
+<div class="color-2"></div>
+<div class="color-3"></div>
+<div class="color-4"></div>
+
+<style>
+	.color {
+		height: 800px;
+		width: 800px;
+		background-image: radial-gradient(closest-side, #14ac81, #003840);
+		opacity: 50%;
+		position: absolute;
+		top: -600px;
+		left: 15%;
+	}
+
+	.color-2 {
+		height: 700px;
+		width: 700px;
+		background-image: radial-gradient(closest-side, #14ac81, #003840);
+		opacity: 50%;
+		position: absolute;
+		top: 0px;
+		left: -600px;
+		overflow: hidden;
+	}
+
+	.color-3 {
+		height: 700px;
+		width: 700px;
+		background-image: radial-gradient(closest-side, white, #003840);
+		opacity: 30%;
+		position: absolute;
+		top: -500px;
+		right: -400px;
+	}
+
+	.color-4 {
+		height: 700px;
+		width: 700px;
+		background-image: radial-gradient(closest-side, white, #003840);
+		opacity: 20%;
+		position: absolute;
+		top: 200px;
+		right: -600px;
+	}
+</style>
