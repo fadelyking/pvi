7fa740ed3dc2830239bde938e2ad0e7d52a975da
fadelyking <fadely195@gmail.com>
2024-09-09 02:37:20 +0300

    gradient button flowbite

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,10 +1,11 @@
-<button on:click class=" drop-shadow-xl"><slot>Button</slot></button>
+<script>
+	// @ts-nocheck
 
-<style>
-	button {
-		border-radius: 16;
-		padding: 10px 30px 10px 30px;
-		background-color: black;
-		border-radius: 16px;
-	}
-</style>
+	import { GradientButton } from "flowbite-svelte";
+
+	export let color = "";
+</script>
+
+<GradientButton on:click {color} class=" px-12 py-3 rounded-xl glow-effect"
+	>Click</GradientButton
+>
