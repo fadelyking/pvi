e0982bdb1e5a8adf3c01e73cdae583785713a34a
fadelyking <fadely195@gmail.com>
2024-09-09 16:13:16 +0300

    unique id for each record

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,6 +1,7 @@
-import { pgTable, timestamp, text } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, text, serial } from "drizzle-orm/pg-core";
 
 export const clicksTable = pgTable("clicks", {
+	id: serial("id").primaryKey(),
 	country: text("name"),
 	created_at: timestamp("created_at"),
 });
