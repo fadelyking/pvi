70cc64894e572a7142b72c1fb969dc001f5a7b3c
satanskitty <satanskitty@protonmail.ch>
2024-09-10 12:39:44 +0200

    Form component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,11 @@
 	import Country from "$lib/components/Country.svelte";
 	import ProgressBar from "$lib/components/ProgressBar.svelte";
 	import { Ps, Il } from "svelte-flag-icons";
-	import type { PageData/*, FormData*/ } from "./$types";
-	import { incrementByISO } from "$lib/stores/clicks";
+	import type { PageData /*, FormData*/ } from "./$types";
 	import Button from "$lib/components/Button.svelte";
 	import { enhance } from "$app/forms";
+
+	import Form from "$lib/components/Form.svelte";
 
 	export let data: PageData;
 	// export let form: FormData;
@@ -18,36 +19,17 @@
 </script>
 
 <div class="grid h-screen place-items-center grid-cols-3 gap-32">
-	<div class="flex flex-col items-center">
-		<Ps size={128} />
-		<Country countryISO="PS" clicks={data.palestine.length} />
-
-		<form method="POST" action="?/click" use:enhance>
-			<input type="hidden" name="iso" value="PS" />
-			<Button
-				classNames="bg-gradient-to-br from-green-400 px-12 my-4 hover:shadow-xl transition hover:saturate-150 active:scale-90"
-				on:click={() => incrementByISO("PS")}
-			/>
-		</form>
-	</div>
+	<Form countryISO="PS" clicks={data.palestine.length} />
 
 	<div class="flex flex-col items-center gap-3">
-		<span class="text-center font-black text-5xl px-3">{Intl.NumberFormat(undefined).format(totalClicked)} clicks</span>
-		<br/>
+		<span class="text-center font-black text-5xl px-3"
+			>{Intl.NumberFormat(undefined).format(totalClicked)} clicks</span
+		>
+		<br />
 		<ProgressBar />
 	</div>
 
-	<div class="flex flex-col items-center">
-		<Il size={128} />
-		<Country countryISO="IL" clicks={data.israel.length} />
-		<form method="POST" action="?/click" use:enhance>
-			<input type="hidden" name="iso" value="IL" />
-			<Button
-				classNames="bg-gradient-to-br from-blue-700 px-12 my-4 hover:shadow-xl transition hover:saturate-150 active:scale-90"
-				on:click={() => incrementByISO("IL")}
-			/>
-		</form>
-	</div>
+	<Form countryISO="IL" clicks={data.israel.length} />
 </div>
 
 <div class="color"></div>
