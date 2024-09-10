eceb1e4668b715f5cbf610668365625d049ef299
fadelyking <fadely195@gmail.com>
2024-09-10 18:42:27 +0300

    progress schema

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,9 +1,15 @@
-import { pgTable, timestamp, text, serial } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, text, serial, numeric } from "drizzle-orm/pg-core";
 
 export const clicksTable = pgTable("clicks", {
 	id: serial("id").primaryKey(),
 	country: text("name"),
 	created_at: timestamp("created_at"),
+});
+
+export const progressTable = pgTable("progress", {
+	id: serial("id").primaryKey(),
+	progress: numeric("progress"),
+	last_updated: timestamp("last_updated").defaultNow(),
 });
 
 export type InsertClick = typeof clicksTable.$inferInsert;
