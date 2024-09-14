25575200669b5c40ca79241ba30cde37ba0d185b
fadelyking <fadely195@gmail.com>
2024-09-14 05:53:23 +0300

    donation box 1/4

--- src/lib/components/Donate.svelte
+++ src/lib/components/Donate.svelte
@@ -1,8 +1,49 @@
-<script>
-	// TODOS:
-	// DONATION BOX THAT POPS UP ON CLICK (REFERENCE BELOW)
-	// PART 1
-	// https://imgur.com/OO4P6tu
-	// PART 2
-	// https://imgur.com/EFX5BIA
-</script>
+<div
+	class="pointer-events-auto bg-[#0e696a] rounded-lg shadow-lg max-w-sm mx-auto w-full p-4 md:p-6 pt-6 -mt-24"
+>
+	<div class="flex flex-col items-center justify-center">
+		<span class="text-2xl font-black uppercase">Get Clicks Now </span>
+		<span class="mt-4 font-bold">1$ for 1000 Clicks</span>
+	</div>
+	<form class="grid grid-cols-2 gap-2 mt-2">
+		<div class="flex flex-col gap-2">
+			<button class="form-button">1000 Clicks</button><button
+				class="form-button">10000 Clicks</button
+			>
+		</div>
+		<div class="flex flex-col gap-2">
+			<button class="form-button">50000 Clicks</button><button
+				class="form-button">100000 Clicks</button
+			>
+		</div>
+		<input
+			class="col-span-2 rounded-md text-black"
+			type="text"
+			maxlength="6"
+			name="other-amount"
+			placeholder="Other amount"
+		/>
+		<img
+			src="twig-divider.png"
+			alt="divider"
+			class="col-span-2 my-6 w-full"
+		/>
+		<button
+			class="col-span-2 px-6 py-4 bg-green-600 text-white font-black rounded-full mb-4"
+			type="submit">Next</button
+		>
+	</form>
+</div>
+
+<style>
+	.form-button {
+		background-color: #02a676;
+		border-width: 1px;
+		border-color: #007369;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 0.375rem;
+		height: 3rem;
+	}
+</style>
