c4c25e430f84bec24c656ccce3b0cd7ae8446071
maya <maya@mayas-mbp.home>
2024-09-15 15:26:59 +0200

    layout and ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import "../app.css";
+	// TODO: Make background change dynamically based on winning country dominant colors from flag
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,25 +9,21 @@
 	$: totalClicked = data.clicks.length;
 </script>
 
-<div class="flex flex-col justify-center items-center">
-	<div
-		class="grid h-screen place-items-center grid-rows-3 md:grid-rows-none md:grid-cols-3 gap-0 md:gap-32 max-w-screen-xl w-full"
-	>
-		<Form countryISO="PS" clicks={data.palestine.length} />
-
-		<div class="flex flex-col items-center gap-3">
-			<span class="text-center font-black text-5xl px-3"
-				>{Intl.NumberFormat(undefined).format(totalClicked)} clicks</span
-			>
-			<br />
-			<ProgressBar
-				progress={data.progress[0]?.progress !== null
-					? Math.round(parseInt(data.progress[0].progress))
-					: 50}
-			/>
+<div class="grid grid-cols-1 justify-center place-items-center md:grid-cols-2">
+	<div class="flex flex-col gap-3">
+		<div class="flex flex-row gap-3">
+			<Form countryISO="PS" clicks={data.palestine.length} />
+			<Form countryISO="IL" clicks={data.israel.length} />
 		</div>
-
-		<Form countryISO="IL" clicks={data.israel.length} />
+		<p class=" font-black text-5xl px-3">
+			{Intl.NumberFormat(undefined).format(totalClicked)} clicks
+		</p>
+		<ProgressBar
+			progress={data.progress[0]?.progress !== null
+				? Math.round(parseInt(data.progress[0].progress))
+				: 50}
+		/>
 	</div>
+
 	<Donate />
 </div>
