573d51fa4117490254866e157dcdb8f4f555f9af
fadelyking <fadely195@gmail.com>
2024-10-04 07:37:46 +0300

    remove console logs change from country name to country ISO add new action for donation form

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,8 +5,7 @@ import { clicksTable, donorsTable, progressTable } from "$lib/server/schema";
 import type { PageServerLoad } from "./$types";
 import { ISOToName } from "$lib/helpers/ISOToName";
 
-export const load: PageServerLoad = async ({url}) => {
-
+export const load: PageServerLoad = async ({ url }) => {
 	// By default, we return Palestine and Israel if no query params are defined.
 	const firstCountry = url.searchParams.get("q1") ?? "PS";
 	const secondCountry = url.searchParams.get("q2") ?? "IL";
@@ -14,11 +13,10 @@ export const load: PageServerLoad = async ({url}) => {
 	const firstCountryFriendly = ISOToName(firstCountry);
 	const secondCountryFriendly = ISOToName(secondCountry);
 
-	console.log(firstCountryFriendly);
-	console.log(secondCountryFriendly);
-
 	if (!firstCountryFriendly || !secondCountryFriendly) {
-		throw Error("Server-sided error occured, our monkeys are hard at work to figure out why.");
+		throw Error(
+			"Server-sided error occured, our monkeys are hard at work to figure out why."
+		);
 	}
 
 	return {
@@ -58,7 +56,7 @@ export const actions = {
 		if (iso === "PS") {
 			try {
 				await db.insert(clicksTable).values({
-					country: "Palestine",
+					country: "PS",
 					created_at: new Date(),
 				});
 				currentProgress = Math.min(currentProgress + 0.1, 100);
@@ -74,7 +72,7 @@ export const actions = {
 		} else {
 			try {
 				await db.insert(clicksTable).values({
-					country: "Israel",
+					country: "IL",
 					created_at: new Date(),
 				});
 				currentProgress = Math.min(currentProgress - 0.1, 100);
@@ -82,11 +80,44 @@ export const actions = {
 					progress: currentProgress.toString(),
 					last_updated: new Date(),
 				});
-				return { success: true, message: "Palestine entry added" };
+				return { success: true, message: "Israel entry added" };
 			} catch (error) {
 				console.error("Database insert error: ", error);
 				return { success: false, error: "Failed to insert data" };
 			}
+		}
+	},
+	donate: async ({ request }) => {
+		const formData = await request.formData();
+
+		const displayName = formData.get("display-name");
+		const email = formData.get("email");
+		const phoneNumber = formData.get("mobile-phone");
+		const amount = parseFloat(formData.get("amount"));
+		const country = formData.get("country");
+		const message = formData.get("message");
+		const receiveUpdates = formData.has("receive-update");
+		const anonymous = formData.has("anonymous");
+
+		try {
+			await db.insert(donorsTable).values({
+				name: displayName,
+				email: email,
+				phone_number: phoneNumber,
+				amount: amount,
+				message: message,
+				country: country,
+				receive_updates: receiveUpdates,
+				anonymous: anonymous,
+			});
+			console.log("Donation record inserted successfully.");
+			return {
+				success: true,
+				message: "Donation submitted successfully.",
+			};
+		} catch (error) {
+			console.error("Database insert error: ", error);
+			return { success: false, error: "Failed to insert donation data" };
 		}
 	},
 };
