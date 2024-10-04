8800dfece633bd1ba9f31b487617e1c7b267cc68
fadelyking <fadely195@gmail.com>
2024-10-04 07:36:49 +0300

    remove dummy data and switch to db data

--- src/lib/components/Leaderboard.svelte
+++ src/lib/components/Leaderboard.svelte
@@ -1,63 +1,29 @@
-<script>
-    import { FormatTimeRelative } from "$lib/helpers/FormatTimeRelative";
+<script lang="ts">
+	import { FormatTimeRelative } from "$lib/helpers/FormatTimeRelative";
 	import { ISOToName } from "$lib/helpers/ISOToName";
 
-	const leaderboard = [
-		{
-			name: "Benjamin",
-			amount: 10000,
-			country: "IL",
-		},
-		{
-			name: "Furkan",
-			amount: 5000,
-			country: "IL",
-		},
-		{
-			name: "Fadhel",
-			amount: 10000,
-			country: "PS",
-		},
-	];
-
-	const recent = [
-		{
-			name: "Person",
-			amount: 500,
-			country: "IL",
-			date: Date.now()
-		},
-		{
-			name: "Person 2",
-			amount: 5000,
-			country: "IL",
-			date: Date.now() - 150000,
-		},
-		{
-			name: "Person 3",
-			amount: 10000,
-			country: "PS",
-			date: Date.now() - 550000,
-		},
-	];
+	export let donors;
 </script>
 
 <div class="bg-[#0e696a] rounded-lg p-6 w-[512px]">
 	<h2 class="font-semibold">Leaderboards</h2>
 	<ul>
-		{#each leaderboard as donor}
+		{#each donors as donor}
 			<li>
-				<strong>{donor.name}</strong> donated {donor.amount.toLocaleString()} clicks for {ISOToName(
-					donor.country,
-				)}
+				<strong>{donor.name}</strong> donated {donor.amount.toLocaleString()}
+				clicks for {ISOToName(donor.country)}
 			</li>
 		{/each}
 	</ul>
-	<hr/>
+	<hr />
 	<h2 class="font-semibold mt-2">Most Recent</h2>
 	<ul>
-		{#each recent as donor}
-		<li><strong>{donor.name}</strong> donated {donor.amount} clicks {FormatTimeRelative(new Date(donor.date))}</li>
+		{#each donors as donor}
+			<li>
+				<strong>{donor.name}</strong> donated {donor.amount} clicks {FormatTimeRelative(
+					new Date(donor.date)
+				)}
+			</li>
 		{/each}
 	</ul>
 </div>
