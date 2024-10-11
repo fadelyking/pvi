96992899b7bbe4b5047c36de5c5b8ce3d3eed003
fadelyking <fadely195@gmail.com>
2024-10-11 21:15:17 +0300

    simplify leaderboard to include icons

--- src/lib/components/Leaderboard.svelte
+++ src/lib/components/Leaderboard.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
 	import { FormatTimeRelative } from "$lib/helpers/FormatTimeRelative";
 	import { ISOToName } from "$lib/helpers/ISOToName";
-
+	let flagElement: HTMLElement;
 	export let donors;
 </script>
 
-<div class="border border-white/20 rounded-lg p-4 h-full">
+<div class="rounded-lg p-4 h-full">
 	<h2 class="font-semibold text-center text-xl mb-2">Leaderboards</h2>
 	<ul class="text-sm">
-		{#each donors as donor, i}
-			<li class="odd:bg-black/15 p-1 rounded-md">
-				{i + 1}. <strong>{donor.name}</strong> {donor.amount.toLocaleString()} {ISOToName(donor.country)}
+		{#each donors as donor}
+			<li class="odd:bg-black/15 p-1 rounded-md flex items-center gap-2">
+				<img
+					bind:this={flagElement}
+					class="select-none"
+					id="flag"
+					width={30}
+					height={30}
+					src="/flags/1x1/{donor.country.toLowerCase()}.svg"
+					alt="Flag"
+				/>
+				<strong>{donor.name}</strong>
+				{donor.amount.toLocaleString()}
+				{ISOToName(donor.country)}
 			</li>
 		{/each}
 	</ul>
