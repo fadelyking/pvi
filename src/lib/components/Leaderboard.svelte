516c56b6aec905d173577cc3cc18168471193aa3
fadelyking <fadely195@gmail.com>
2024-10-11 21:42:51 +0300

    leaderboard messages

--- src/lib/components/Leaderboard.svelte
+++ src/lib/components/Leaderboard.svelte
@@ -5,7 +5,7 @@
 	export let donors;
 </script>
 
-<div class="rounded-lg p-4 h-full">
+<div class="rounded-lg p-4 h-1/2">
 	<h2 class="font-semibold text-center text-xl mb-2">Leaderboards</h2>
 	<ul class="text-sm">
 		{#each donors as donor}
@@ -25,15 +25,31 @@
 			</li>
 		{/each}
 	</ul>
-	<!-- <hr />
-	<h2 class="font-semibold mt-2">Most Recent</h2>
-	<ul>
-		{#each donors as donor}
-			<li>
-				<strong>{donor.name}</strong> donated {donor.amount} clicks {FormatTimeRelative(
-					new Date(donor.date)
-				)}
-			</li>
-		{/each}
-	</ul> -->
+</div>
+<div class="rounded-lg h-full">
+	<h2 class="font-semibold text-center text-xl mb-2">Messages</h2>
+	<div class="border border-white/20 rounded-lg p-4 h-1/2 overflow-auto">
+		<ul class="text-sm">
+			{#each donors as donor}
+				<li
+					class="odd:bg-black/15 p-1 rounded-md flex flex-col gap-3 items-start"
+				>
+					<div class="flex gap-3 items-center">
+						<img
+							bind:this={flagElement}
+							class="select-none"
+							id="flag"
+							width={30}
+							height={30}
+							src="/flags/1x1/{donor.country.toLowerCase()}.svg"
+							alt="Flag"
+						/>
+
+						<strong>{donor.name}:</strong>
+					</div>
+					<span class="w-56">{donor.message}</span>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
